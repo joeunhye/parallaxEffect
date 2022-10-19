8ea557e390ece3c65b4f9f5c9cb9c340293f1fd2
eunhye919 <eunhye919@vtex.co.kr>
2022-10-19 10:40:41 +0900

    text effect 수정

--- parallax-jq.js
+++ parallax-jq.js
@@ -13,14 +13,6 @@ $(window).scroll(function () {
 	let scrollTop = $(window).scrollTop();
 
 	// 메뉴 활성화 효과
-	//for문
-	// for (let i = 0; i <= 8; i++) {
-	// 	if (scrollTop >= $(".content__item").eq(i).offset().top) {
-	// 		$("#parallax__dot ul li").eq(i).addClass("active").siblings().removeClass("active");
-	// 	}
-	// }
-
-	//each()
 	$(".content__item").each(function (index, ele) {
 		if (scrollTop >= $(".content__item").eq(index).offset().top - 2) {
 			$("#parallax__nav ul li").eq(index).addClass("active").siblings().removeClass("active");
@@ -28,11 +20,11 @@ $(window).scroll(function () {
 		}
 	});
 
-	//메뉴 사라짐 효과(pc)
+	//메뉴 사라짐 효과 1
 	// if (scrollTop > 0) $("#parallax__nav").addClass("show");
 	// else $("#parallax__nav").removeClass("show");
 
-	//메뉴 사라짐 효과(mobile)
+	//메뉴 사라짐 효과 2
 	nowScrollTop = true;
 
 	setTimeout(function () {
@@ -43,33 +35,6 @@ $(window).scroll(function () {
 	}, 1000);
 
 	//이질감 효과
-	// let offset1 = scrollTop - $("#section1").offset().top;
-	// let offset2 = scrollTop - $("#section2").offset().top;
-	// let offset3 = scrollTop - $("#section3").offset().top;
-	// let offset4 = scrollTop - $("#section4").offset().top;
-	// let offset5 = scrollTop - $("#section5").offset().top;
-	// let offset6 = scrollTop - $("#section6").offset().top;
-	// let offset7 = scrollTop - $("#section7").offset().top;
-	// let offset8 = scrollTop - $("#section8").offset().top;
-	// let offset9 = scrollTop - $("#section9").offset().top;
-	// $("#section1").css({ transform: `translateX(${offset1}px)` });
-	// $("#section2").css({ transform: `translateX(${offset2}px)` });
-	// $("#section3").css({ transform: `translateX(${offset3}px)` });
-	// $("#section4").css({ transform: `translateX(${offset4}px)` });
-	// $("#section5").css({ transform: `translateX(${offset5}px)` });
-	// $("#section6").css({ transform: `translateX(${offset6}px)` });
-	// $("#section7").css({ transform: `translateX(${offset7}px)` });
-	// $("#section8").css({ transform: `translateX(${offset8}px)` });
-	// $("#section9").css({ transform: `translateX(${offset9}px)` });
-
-	//for문
-	// for (let i = 1; i <= 9; i++) {
-	// 	let offset = scrollTop - $(`#section${i}`).offset().top;
-	// 	$(`#section${i}`).css({ transform: `translateX(${offset}px)` });
-	// 	console.log(offset);
-	// }
-
-	//each()
 	$(".content__item").each(function (i) {
 		let offset1 = (scrollTop - $(this).offset().top) * 0.2;
 		let offset2 = (scrollTop - $(this).offset().top) * 0.1;
@@ -85,17 +50,13 @@ $(window).scroll(function () {
 	});
 
 	//글씨 나타나기
-	// for문
-	for (let i = 1; i <= 9; i++) {
-		if (scrollTop >= $(`#section${i}`).offset().top) {
-			console.log(i);
-			$(`#section${i} .content__item__desc span`).each(function (i) {
-				setTimeout(function () {
-					$(`#section${i} .content__item__desc span`).eq(i).addClass("show");
-				}, 50 * i);
+	$('.content__item').each(function(i) {
+		if(scrollTop >= $(this).offset().top) {
+			$(this).find('.content__item__desc span').each(function (j) {
+				$(this).addClass('show').css('transition-delay', `${0.05*j}s`);
 			});
 		}
-	}
+	})
 });
 
 function hasScroll() {
